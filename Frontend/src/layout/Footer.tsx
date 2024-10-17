00237d93ccd2e1c250ac5970f5f07823b0c353c1
jerryChas <jerry.olsson@chasacademy.se>
2024-10-17 14:02:12 +0200

    check user status button in footer

--- Frontend/src/layout/Footer.tsx
+++ Frontend/src/layout/Footer.tsx
@@ -1,7 +1,28 @@
 function Footer() {
+  const checkUser = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/auth/status", {
+        credentials: "include", // This line adds the credentials
+      });
+      if (!response.ok) {
+        throw new Error("Failed to check user credentials");
+      }
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <footer className="bg-green-600 text-white text-center p-4">
+    <footer className="relative p-4 text-center text-white bg-green-600">
       <p>&copy; 2024 Monkey Emails Corp. All rights reserved.</p>
+      <button
+        onClick={checkUser}
+        className="absolute rounded p-1 hover:shadow-md  border top-4 left-3"
+      >
+        Check user status
+      </button>
     </footer>
   );
 }
