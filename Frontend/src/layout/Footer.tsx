445bc79df588338d28792d1c1eef876a13db1f7f
Emil Högberg <emil.hogberg@gmail.com>
2024-11-13 12:15:35 +0100

    Updated design of check user status button

--- Frontend/src/layout/Footer.tsx
+++ Frontend/src/layout/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   const checkUser = async () => {
     try {
       const response = await fetch(`${envMode()}/auth/status`, {
-        credentials: "include", // This line adds the credentials
+        credentials: "include",
       });
       if (!response.ok) {
         throw new Error("Failed to check user credentials");
@@ -17,14 +17,18 @@ function Footer() {
   };
 
   return (
-    <footer className="relative p-4 text-center text-white bg-green-600">
-      <p>&copy; 2024 Monkey Emails Corp. All rights reserved.</p>
-      <button
-        onClick={checkUser}
-        className="absolute rounded p-1 hover:shadow-md  border top-4 left-3"
-      >
-        Check user status
-      </button>
+    <footer className="relative p-4 text-white bg-green-600 flex items-center justify-center">
+      <div>
+        <button
+          onClick={checkUser}
+          className="bg-green-500 text-white rounded px-4 py-2 hover:bg-green-700"
+        >
+          Check User Status
+        </button>
+      </div>
+      <p className="mx-auto">
+        &copy; 2024 Monkey Emails Corp. All rights reserved.
+      </p>
     </footer>
   );
 }
