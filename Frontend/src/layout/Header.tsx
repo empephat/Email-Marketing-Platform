60dd29d7bec7d1f69bf46f9a742c2f38f77b4fb2
Alicia Karlsson <aliciaxd@live.se>
2024-11-15 14:39:56 +0100

    small changes

--- Frontend/src/layout/Header.tsx
+++ Frontend/src/layout/Header.tsx
@@ -1,6 +1,5 @@
 import envMode from "@/components/helper/checkENVmode";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import { User } from 'lucide-react'
 import { useAuth } from "@/hooks/useAuth";
 
